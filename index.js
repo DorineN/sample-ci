779ae6a2c447d5c6dbe43e7ed413fd02cbf906fd
Dorine <do@gmail.com>
2016-06-01 17:08:44 +0200

    updated index

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ api.get("/", function(req, res, next){
     res.send('Hello World');
 });
 
-console.log('Server started');
-api.listen(3000);
+var port = process.env.PORT || DEFAULT_PORT;
+Console.log('server started on port ' + port);
+api.listen(port);
 
 module.exports = api;
