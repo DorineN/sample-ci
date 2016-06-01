6e6dd3c8721debe96da86e3b453ae357cc280b56
Dorine <do@gmail.com>
2016-06-01 17:26:29 +0200

    Udpated index with all requests

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var api = express();
 
-api.get("/", function(req, res, next){
-    res.send('Hello World');
+api.get("/contacts", function(req, res, next){
+    res.send([]);
+});
+
+api.post("/contacts/:name", function(req, res, next){
+    res.send();
+});
+
+api.put("/contacts/:name", function(req, res, next){
+    if(req.params.name == 'exist')
+        return res.status(403).exist();
+    res.send();
+});
+
+api.get("//contacts/:name/:new", function(req, res, next){
+    res.send();
+});
+
+api.get("//contacts/:name", function(req, res, next){
+    res.send();
 });
 
 var port = process.env.PORT || DEFAULT_PORT;
